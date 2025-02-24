bf6f620c183433ec9c0fd222f697baa44aa6656e
wamisho-d <166142918+wamisho-d@users.noreply.github.com>
2025-02-23 22:07:54 -0500

    Update App.jsx

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import NotFound from './components/NotFound';
 
 const App = () => {
     return (
         <Router>
-            <Switch>
+            <Routes>
                 <Route exact path="/" component={HomePage} />
                 <Route component={NotFound} />
-            </Switch>
+            </Routes>
         </Router>
     );
 };
